bd668660d17bb302210f7153c2b9925aeb0a6e40
Bassem Hassan Kamal <bassem.hassan1995@gmail.com>
2023-01-07 19:03:30 +0200

    add ltr support

--- app/src/main/java/bassem/udacity/session_1/MainActivity.kt
+++ app/src/main/java/bassem/udacity/session_1/MainActivity.kt
@@ -1,51 +1,30 @@
 package bassem.udacity.session_1
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import bassem.udacity.session_1.databinding.ActivityMainBinding
+import bassem.udacity.session_1.databinding.ActivityMainLtrBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //    private lateinit var binding: ActivityMainLtrBinding
-    private lateinit var binding: ActivityMainBinding
+            private lateinit var binding: ActivityMainLtrBinding
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        bindViewsUsingFindViewById()
-
-//        binding = ActivityMainLtrBinding.inflate(layoutInflater)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainLtrBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
-        bindViewsUsingViewBinding()
-
+        bindViews()
     }
 
-    private fun bindViewsUsingFindViewById() {
-
-        val sessionTextView = findViewById<TextView>(R.id.session_tv)
-        val helloTextView = findViewById<TextView>(R.id.hello_tv)
-        val startButton = findViewById<Button>(R.id.start_btn)
-
-        sessionTextView.text = getString(R.string.session_1)
-        helloTextView.text = getString(R.string.welcome_to_fwd)
-        startButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                getString(R.string.welcome_to_fwd), Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-
-    private fun bindViewsUsingViewBinding() {
+    private fun bindViews() {
         with(binding) {
             sessionTv.text = getString(R.string.session_1)
-            helloTv.text = getString(R.string.welcome_to_fwd)
+            helloTv.text = getString(R.string.hello_world)
             startBtn.setOnClickListener {
                 Toast.makeText(
                     this@MainActivity,
